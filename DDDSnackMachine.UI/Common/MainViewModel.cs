2dbe5047e4eb33c4aacb06b941741df77daa8a6f
ahmed mohamed marzouk <ahmedmarzouk.dh@gmail.com>
2018-01-15 23:53:55 +0200

    Adding UI basics

---
+++ DDDSnackMachine.UI/Common/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SnackMachineATMAutomation.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDSnackMachine.UI.Common
+{
+    public class MainViewModel : ViewModel
+    {
+        public MainViewModel()
+        {
+            var viewModel = new SnackMachineViewModel(new SnackMachine());
+            _dialogService.ShowDialog(viewModel);
+        }
+    }
+}
---
+++ DDDSnackMachine.UI/SnackMachineViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DDDSnackMachine.UI.Common;
+using SnackMachineATMAutomation.Logic;
+
+namespace DDDSnackMachine.UI
+{
+    public class SnackMachineViewModel : ViewModel
+    {
+        private readonly SnackMachine _snackMachine;
+
+        public override string Caption => "Snack Machine";
+
+        public SnackMachineViewModel(SnackMachine snackMachine)
+        {
+            _snackMachine = snackMachine;
+        }
+    }
+}
